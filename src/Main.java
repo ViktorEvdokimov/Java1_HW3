8e343eb2df97d14cca438cdd6d1012d65b05c0e5
EvdokimovViktor <evdokimov.viktor.91@mail.ru>
2020-12-26 09:09:00 +0500

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        taskOne();
+    }
+    static void taskOne (){
+        int ans = (int) (Math.random()*10);
+        boolean end;
+        for (int i=3; i>0; i--) {
+            end = check(i, ans);
+            if (end) break;
+            //          if (end==false || i==1) System.out.println("Вы проиграли. Загаданное число " + ans + " .");
+        }
+
+    }
+    static boolean check (int i, int ans){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a==ans){
+            System.out.println("Вы угадали!");
+            return true;
+        }
+        if (a<ans){
+            System.out.println("Загаданное число больше введенного. Осталось попыток " + i + " .");
+            return false;
+        }
+        System.out.println("Загаданное число меньше введенного. Осталось попыток " + i + " .");
+        return false;
+    }
 }
